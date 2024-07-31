337ea97a71876c9f4cee3135406b12893cf944b1
Ozzeek <osama.m.alnagar@gmail.com>
2024-08-01 02:40:07 +0300

    feat: tab navigation and logo placement

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -16,103 +16,63 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Explore = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.tabComponent}>
+      <Text>Explore!</Text>
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const Repositories = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.tabComponent}>
+      <Text>Repositoris!</Text>
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const Tab = createMaterialTopTabNavigator();
+
+const App = () => {
+  return (
+    <View style={styles.topBar}>
+      <View style={styles.logoBar}>
+        <Image
+          style={{width: 112, objectFit: 'contain'}}
+          source={require('./src/assets/images/logo.png')}
+        />
+      </View>
+      <NavigationContainer style={{flex: 1}}>
+        <Tab.Navigator>
+          <Tab.Screen name="Explore" component={Explore} />
+          <Tab.Screen name="Repositories" component={Repositories} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  topBar: {
+    paddingTop: 44,
+    flex: 1,
+  },
+  logoBar: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 24,
+  },
+  tabComponent: {
+    backgroundColor: '#FAFCFE',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
