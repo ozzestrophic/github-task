46c4e898904edfa4dfc76f7f6297ada31302429b
Ozzeek <osama.m.alnagar@gmail.com>
2024-08-02 00:50:02 +0300

    setup function to fetch repos

--- src/redux/exploreSlice.js
+++ src/redux/exploreSlice.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const initialState = {
   loading: false,
   exploreRepos: [
@@ -14,20 +16,43 @@ const initialState = {
       starCount: 12,
     },
   ],
-  error: '',
+  error: null,
 };
+
+const FETCH_EXPLORE_REPOS_REQUEST = 'FETCH_EXPLORE_REPOS_REQUEST';
+const FETCH_EXPLORE_REPOS_SUCCESS = 'FETCH_EXPLORE_REPOS_SUCCESS';
+const FETCH_EXPLORE_REPOS_FAILURE = 'FETCH_EXPLORE_REPOS_FAILURE';
 
 const exploreReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_EXPLORE_REPOS_REQUEST':
-      return {...state, loading: true};
-    case 'FETCH_EXPLORE_REPOS_SUCCESS':
-      return [...state, action.payload];
-    case 'FETCH_EXPLORE_REPOS_FAILURE':
-      return {...state, error: action.payload};
+    case FETCH_EXPLORE_REPOS_REQUEST:
+      return {...state, loading: true, error: null};
+    case FETCH_EXPLORE_REPOS_SUCCESS:
+      return {...state, loading: false, exploreRepos: action.payload};
+    case FETCH_EXPLORE_REPOS_FAILURE:
+      return {...state, loading: false, error: action.payload};
     default:
       return state;
   }
 };
 
 export default exploreReducer;
+
+export const fetchExploreRepos = (limit = 10, date = '', language = '') => {
+  return async dispatch => {
+    dispatch({type: FETCH_EXPLORE_REPOS_REQUEST});
+    try {
+      const query = `q=${date && `created:>${date}+`}
+        ${language && `language:${language}&`}
+        sort=stars&order=desc
+        ${limit && `&per_page=${limit}`}
+        `;
+      const response = await axios.get(
+        `https://api.github.com/search/repositories${query}`,
+      );
+      console.log(response.data);
+    } catch (error) {
+      dispatch({type: FETCH_EXPLORE_REPOS_FAILURE, payload: error.message});
+    }
+  };
+};
