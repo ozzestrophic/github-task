c4f134e47a5d7c184d1f685183ea4fe2bae989b4
Ozzeek <osama.m.alnagar@gmail.com>
2024-08-01 23:26:32 +0300

    feat: added store provider to app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {Image, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
@@ -12,25 +5,29 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import Explore from './src/screens/Explore';
 import Repositories from './src/screens/Repositories';
 import styles from './src/styles/theme';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
 
 const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
-    <View style={styles.main}>
-      <View style={styles.logoBar}>
-        <Image
-          style={styles.logoImage}
-          source={require('./src/assets/images/logo.png')}
-        />
+    <Provider store={store}>
+      <View style={styles.main}>
+        <View style={styles.logoBar}>
+          <Image
+            style={styles.logoImage}
+            source={require('./src/assets/images/logo.png')}
+          />
+        </View>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Explore" component={Explore} />
+            <Tab.Screen name="Repositories" component={Repositories} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </View>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Explore" component={Explore} />
-          <Tab.Screen name="Repositories" component={Repositories} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
+    </Provider>
   );
 };
 
