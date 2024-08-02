03c9652a6136181b48409227c66af0209e18058c
Ozzeek <osama.m.alnagar@gmail.com>
2024-08-02 03:03:36 +0300

    display data from repos store

--- src/screens/Repositories.jsx
+++ src/screens/Repositories.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import {Text, View, useColorScheme} from 'react-native';
+import React, {useEffect} from 'react';
+import {ScrollView, Text, View, useColorScheme} from 'react-native';
 import styles from '../styles/theme';
 import {COLORS} from '../utils/style';
+import store from '../redux/store';
+import {fetchRepos} from '../redux/repositoriesSlice';
+import {useSelector} from 'react-redux';
+import TrendingRepoCard from '../components/TrendingRepoCard';
+
+const selectRepositories = state => state.repositories.repos;
 
 const Repositories = () => {
-  // const [colorScheme, setColorScheme] = useState('light');
-  const colorScheme = useColorScheme();
-  const colors = COLORS[colorScheme ?? 'light'];
+  useEffect(() => {
+    store.dispatch(fetchRepos());
+  }, []);
+  const fetchedRepos = useSelector(selectRepositories);
+  const reposFetchState = useSelector(state => state.repositories.loading);
+  const renderedListRepos = fetchedRepos.map((item, index) => (
+    <TrendingRepoCard key={index} repo={item} />
+  ));
+  console.log('fetched is ' + fetchedRepos);
   return (
-    <View
-      style={[
-        styles.tabComponent,
-        {backgroundColor: colors.primary_background},
-      ]}>
-      <Text>Repositoris!</Text>
-    </View>
+    <ScrollView style={styles.tabComponent}>
+      <Text style={styles.title}>Repositories</Text>
+      {reposFetchState && (
+        <View>
+          <Text style={{textAlign: 'center', padding: 10}}>Loading...</Text>
+        </View>
+      )}
+      <View style={styles.reposContainer}>{renderedListRepos}</View>
+    </ScrollView>
   );
 };
 
