fc74d18c795d397c67f3136c536761e052a20d69
Ozzeek <osama.m.alnagar@gmail.com>
2024-08-03 17:36:02 +0300

    replaced modal-datetime-picker with ui-datepicker

--- src/components/ExploreDateModal.jsx
+++ src/components/ExploreDateModal.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import React, {useState} from 'react';
 import styles from '../styles/theme';
+import DateTimePicker from 'react-native-ui-datepicker';
 import {Button, View} from 'react-native';
 
 const ExploreDateModal = ({setDate, setDateModalVisible}) => {
   const hideDatePicker = () => setDateModalVisible(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <View>
       {/* <Button title="Show Date Picker" onPress={showDatePicker} /> */}
-      <DateTimePickerModal
-        isVisible={true}
-        display="inline"
-        mode="date"
-        onConfirm={setDate}
-        onCancel={hideDatePicker}
+      <DateTimePicker
+        mode="single"
+        date={selectedDate}
+        onChange={params => setDate(params.date)}
       />
     </View>
   );
