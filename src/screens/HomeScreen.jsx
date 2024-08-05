42f6bd2cd7dc5c3c839f648ef05e7bb8dc35180a
Ozzeek <osama.m.alnagar@gmail.com>
2024-08-05 03:58:56 +0300

    feat: option to toggle light and dark mode

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Image, View} from 'react-native';
+import {Image, Pressable, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Explore from './Explore';
 import Repositories from './Repositories';
 import styles, {darkColors, lightColors} from '../styles/theme';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faLightbulb} from '@fortawesome/free-regular-svg-icons/faLightbulb';
+import {faMoon} from '@fortawesome/free-regular-svg-icons/faMoon';
+
 const logo = require('../assets/images/logo.png');
 const logoDark = require('../assets/images/logo-dark.png');
-
 const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = () => {
   const colorMode = useSelector(state => state.colorMode.mode);
+  const dispatch = useDispatch();
+  // const [mode, setMode] = useState(colorMode.mode);
   const theme = colorMode === 'light' ? lightColors : darkColors;
+  const toggleMode = () =>
+    dispatch({
+      type: 'SET_COLOR_MODE',
+      payload: colorMode === 'light' ? 'dark' : 'light',
+    });
   return (
     <View style={[styles.main, theme.primary_background]}>
       <View style={styles.logoBar}>
@@ -21,6 +31,16 @@ const HomeScreen = () => {
           style={styles.logoImage}
           source={colorMode === 'light' ? logo : logoDark}
         />
+        <Pressable
+          onPress={() => {
+            toggleMode();
+          }}>
+          <FontAwesomeIcon
+            icon={colorMode === 'dark' ? faLightbulb : faMoon}
+            style={[theme.tertiary_text, styles.logoIcon]}
+            size={24}
+          />
+        </Pressable>
       </View>
       <NavigationContainer>
         <Tab.Navigator
